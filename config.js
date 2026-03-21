472bf6b22f75ecac7b689ce19ddcd712682a3d56
кιяαи ѕєя <105064751+Kiranxer@users.noreply.github.com>
2026-03-21 09:43:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,7 @@ module.exports = {
   token: process.env.BOT_TOKEN,
   mongo: process.env.MONGO_URI,
   groupId: process.env.GROUP_ID,
-  adminId: process.env.ADMIN_ID
+  adminId: process.env.ADMIN_ID,
+  webhookUrl: process.env.WEBHOOK_URL,
+  port: process.env.PORT || 8000
 };
