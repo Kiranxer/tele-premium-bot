fbef7b2bb8589a13dbb7934af9e0f4de4ff75624
кιяαи ѕєя <105064751+Kiranxer@users.noreply.github.com>
2026-03-21 08:32:43 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+  token: process.env.BOT_TOKEN,
+  mongo: process.env.MONGO_URI,
+  groupId: process.env.GROUP_ID,
+  adminId: process.env.ADMIN_ID
+};
